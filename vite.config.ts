34d08b7cf491810152b711f9ee751f8b9116dc28
Angel Meraz <2284513+angelmeraz@users.noreply.github.com>
2026-09-02 18:53:46 +0100

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
@@ -6,5 +7,11 @@ export default defineConfig({
 	plugins: [
 		tailwindcss(),
 		sveltekit()
-	]
+	],
+    resolve: {
+		alias: {
+			// This forces Vite to use the Node-safe fallback bundle instead of the browser bundle during server building
+			'isomorphic-dompurify': path.resolve('./node_modules/isomorphic-dompurify/dist/index.js')
+		}
+	}
 });
